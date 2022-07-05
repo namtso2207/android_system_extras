967c1ef58f0993f1c766a17afa3eed13153f9fe3
Goenjoy Huang <goenjoy@namtso.com>
2022-07-05 14:10:47 +0000

    Add root function support [1/4]
    Change-Id: I78841112ba7bd60161fe4db9984fcc6204cf4c9c
    Signed-off-by: Goenjoy Huang <goenjoy@namtso.com>

--- su/su.cpp
+++ su/su.cpp
@@ -80,8 +80,8 @@ void extract_uidgids(const char* uidgids, uid_t* uid, gid_t* gid, gid_t* gids, i
 }
 
 int main(int argc, char** argv) {
-    uid_t current_uid = getuid();
-    if (current_uid != AID_ROOT && current_uid != AID_SHELL) error(1, 0, "not allowed");
+    //uid_t current_uid = getuid();
+    //if (current_uid != AID_ROOT && current_uid != AID_SHELL) error(1, 0, "not allowed");
 
     // Handle -h and --help.
     ++argv;
